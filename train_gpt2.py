2fa2372810cda6c42848fafc42f24d51625c7d68
gdymind <gdymind@gmail.com>
2024-12-14 00:51:36 +0000

    param sharing between wte and lm_head

--- train_gpt2.py
+++ train_gpt2.py
@@ -96,6 +96,9 @@ class GPT(nn.Module):
             )
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weights sharing scheme: redirect wte weights to lm_head's weights
+        self.transformer.wte.weight = self.lm_head.weight
     
     def forward(self, idx, targets=None):
         # idx is in the shape of (B, T)
