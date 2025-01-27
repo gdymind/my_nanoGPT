8fd55d40538485fe8e18ea65372b2c197f81cd6e
gdymind <gdymind@gmail.com>
2025-01-27 12:12:05 +0000

    micrograd: backprop for neuron

--- autograd/engine.py
+++ autograd/engine.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from graphviz import Digraph
 def trace(root): # using dfs to build a graph
     nodes, edges = set(), set()
@@ -16,7 +14,7 @@ def draw_dot(root):
     nodes, edges = trace(root)
     for n in nodes:
         uid = str(id(n))
-        dot.node(name=uid, label = "{%s | %s | data %.4f | grad %.4f}" % (n._op, n.label, n.data, n.grad), shape='record')
+        dot.node(name=uid, label = "{%s | %s | data %.2f | grad %.2f}" % (n._op, n.label, n.data, n.grad), shape='record')
     for n1, n2 in edges:
         dot.edge(str(id(n1)), str(id(n2)))
     return dot
@@ -25,6 +23,7 @@ class Value:
     def __init__(self, data, _children=(), _op = '', label = ''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None # default: no action for leaf nodes
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -32,8 +31,50 @@ class Value:
     def __repr__(self): # for printing
         return f"Value(data={self.data})"
 
-    def __add__(self, other): # a + b 
-        return Value(self.data + other.data, (self, other), '+')
+    def __add__(self, other): # a + b
+        other = other if isinstance(other, Value) else Value(other) # support cases like "a + 2"
+        out = Value(self.data + other.data, (self, other), '+')
+        def _backward(): # set self.grad and other.grad based on out.grad
+            self.grad += out.grad * 1.0
+            other.grad += out.grad * 1.0
+        out._backward = _backward
+        return out
 
     def __mul__(self, other): # a * b
-        return Value(self.data * other.data, (self, other), '*')
+        other = other if isinstance(other, Value) else Value(other) # support cases like "a * 2"
+        out = Value(self.data * other.data, (self, other), '*')
+        def _backward(): # set self.grad and other.grad based on out.grad
+            self.grad += out.grad * other.data
+            other.grad += out.grad * self.data
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other): # support cases like "2 + a"
+        return self + other
+
+    def __rmul__(self, other): # support cases like "2 * a"
+        return self * other
+
+    def tanh(self):
+        n = self.data
+        t = (math.exp(2*n) - 1) / (math.exp(2*n) + 1)
+        out = Value(t, (self, ), 'tanh')
+        def _backward():
+            self.grad += out.grad * (1 - t**2)
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
