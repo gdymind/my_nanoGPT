82ac7e525242e7d44f7199186db063e207a76daf
gdymind <gdymind@gmail.com>
2025-01-28 04:39:09 +0000

    micrograd: implement MLP

--- autograd/engine.py
+++ autograd/engine.py
@@ -105,3 +105,32 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
+
+
+class Neuron:
+    def __init__(self, nin): # nin: number of inputs
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+    
+    def __call__(self, x):
+        # w * x + b
+        out = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        return out
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs
+
+class MLP:
+    def __init__(self, nin, nouts): # now we have multiple "number of outs"
+        sz = [nin] + nouts # nin as the first element
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
